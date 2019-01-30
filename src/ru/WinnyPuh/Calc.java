6be108d545ba1341b9cccb9efcca18de7032da68
WinnyPuh <coolja@mail.ru>
2019-01-30 22:10:16 +0300

    add deduct

--- src/ru/WinnyPuh/Calc.java
+++ src/ru/WinnyPuh/Calc.java
@@ -1,5 +1,11 @@
 public class Calc{
   public static void main(String[] arg){
     System.out.println("Calculate....");
+    int first = Integer.valueOf(arg[0]);
+    int second = Integer.valueOf(arg[1]);
+    int sum = first + second;
+    int ded = first - second;
+    System.out.println("Sum = " + sum);
+    System.out.println("Deduct = " + ded);
   }
 }
